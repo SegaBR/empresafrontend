719888c568c7c67eec89d21b558836dc73bc281b
SegaBR <40266546+SegaBR@users.noreply.github.com>
2021-09-23 17:19:40 -0300

    Update Config.jsx

--- src/Config.jsx
+++ src/Config.jsx
@@ -1,4 +1,4 @@
 module.exports = {
     //enderecoapi : "http://localhost:3002"
-    enderecoapi : "https://empresaapisega.herokuapp.com/"
+    enderecoapi : "https://empresaapisega.herokuapp.com"
 }
